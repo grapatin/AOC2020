b02c691f5342f207ef36c08c771d6ef1ced9dee2
Per Grapatin <per.grapatin@glooko.com>
2020-12-05 19:40:40 +0100

    newDay cleanup

--- newDay/testinput.ts
+++ newDay/testinput.ts
@@ -5,112 +5,34 @@ export const puzzle1_ex = new Array;
 export const puzzle1_resultex = new Array;
 export const puzzle2_ex = new Array;
 export const puzzle2_resultex = new Array;
-puzzle1_resultex[0] = 36334;
-puzzle1_ex[0] = `
-#######
-#G..#E#
-#E#E.E#
-#G.##.#
-#...#E#     
-#...E.#
-#######`;
+puzzle1_resultex[0] = 1;
+puzzle1_ex[0] = ``;
 
-puzzle1_resultex[1] = 39514;
-puzzle1_ex[1] = `
-#######   
-#E..EG#
-#.#G.E#
-#E.##E#
-#G..#.#
-#..E#.#
-#######`;
+puzzle1_resultex[1] = 1;
+puzzle1_ex[1] = ``;
 
-puzzle1_resultex[2] = 27755;
-puzzle1_ex[2] = `
-#######
-#E.G#.#
-#.#G..#
-#G.#.G#
-#G..#.#
-#...E.#
-#######`;
+puzzle1_resultex[2] = 1;
+puzzle1_ex[2] = ``;
 
-puzzle1_resultex[3] = 28944;
-puzzle1_ex[3] = `
-#######
-#.E...#
-#.#..G#
-#.###.#
-#E#G#G#
-#...#G#
-#######`;
+puzzle1_resultex[3] = 1;
+puzzle1_ex[3] = ``;
 
-puzzle1_resultex[4] = 18740;
-puzzle1_ex[4] = `
-#########
-#G......#
-#.E.#...#
-#..##..G#
-#...##..#
-#...#...#
-#.G...G.#
-#.....G.#
-#########`;
-
+puzzle1_resultex[4] = 1;
+puzzle1_ex[4] = ``;
 
 //Second part
-puzzle2_resultex[0] = 4988;
-puzzle2_ex[0] = `
-#######
-#.G...#
-#...EG#
-#.#.#G#
-#..G#E#
-#.....#
-#######`;
+puzzle2_resultex[0] = 1;
+puzzle2_ex[0] = ``;
 
-puzzle2_resultex[1] = 31284;
-puzzle2_ex[1] = `
-#######
-#E..EG#
-#.#G.E#
-#E.##E#
-#G..#.#
-#..E#.#
-#######`;
+puzzle2_resultex[1] = 1;
+puzzle2_ex[1] = ``;
 
-puzzle2_resultex[2] = 3478;
-puzzle2_ex[2] = `
-#######
-#E.G#.#
-#.#G..#
-#G.#.G#
-#G..#.#
-#...E.#
-#######`;
+puzzle2_resultex[2] = 1;
+puzzle2_ex[2] = ``;
 
-puzzle2_resultex[3] = 6474;
-puzzle2_ex[3] = `
-#######
-#.E...#
-#.#..G#
-#.###.#
-#E#G#G#
-#...#G#
-#######
-`;
+puzzle2_resultex[3] = 1;
+puzzle2_ex[3] = ``;
 
-
-puzzle2_resultex[4] = 1140;
-puzzle2_ex[4] = `
-#########   
-#G......#  
-#.E.#...#
-#..##..G#   
-#...##..#  
-#...#...#  
-#.G...G.#   
-#.....G.#   
-#########
-`;
+puzzle2_resultex[4] = 1;
+puzzle2_ex[4] = ``;
 
