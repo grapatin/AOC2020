e00b9b7b30b849b7a99a5fd2ffad09702a945c32
Per Grapatin <per.grapatin@glooko.com>
2020-12-18 12:49:17 +0100

    18:solved

--- 18/testinput.ts
+++ 18/testinput.ts
@@ -1,12 +1,15 @@
 export const puzzle1_number_of_test = 1;
-export const puzzle2_number_of_test = 0;
+export const puzzle2_number_of_test = 1;
 //First part
 export const puzzle1_ex = new Array;
 export const puzzle1_resultex = new Array;
 export const puzzle2_ex = new Array;
 export const puzzle2_resultex = new Array;
-puzzle1_resultex[0] = 1;
-puzzle1_ex[0] = ``;
+puzzle1_resultex[0] = 26 + 437 + 12240 + 13632;
+puzzle1_ex[0] = `2 * 3 + (4 * 5)
+5 + (8 * 3 + 9 + 3 * 4 * 3)
+5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
+((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2`;
 
 puzzle1_resultex[1] = 1;
 puzzle1_ex[1] = ``;
@@ -21,8 +24,12 @@ puzzle1_resultex[4] = 1;
 puzzle1_ex[4] = ``;
 
 //Second part
-puzzle2_resultex[0] = 1;
-puzzle2_ex[0] = ``;
+puzzle2_resultex[0] = 51 + 46 + 1445 + 669060 + 23340;
+puzzle2_ex[0] = `1 + (2 * 3) + (4 * (5 + 6))
+2 * 3 + (4 * 5)
+5 + (8 * 3 + 9 + 3 * 4 * 3)
+5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
+((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2`;
 
 puzzle2_resultex[1] = 1;
 puzzle2_ex[1] = ``;
